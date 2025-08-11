cef532227a005cbc276d67cd7a0ff851f1214cbc
decaquestionmarks <nateyheit@gmail.com>
2025-08-10 19:35:57 -0700

    getToken

--- dictInterpreter.py
+++ dictInterpreter.py
@@ -6,20 +6,42 @@ def getToken(file)->str:
     curr = ''
     while True:
         curr = file.read(1)
-        if(curr=='\\'):
-            #deal with comment
-            pass
-        if(curr=='"'):
+        if(curr=='/'):
+            curr = file.read(1)
+            if curr == "/":
+                while curr!='\n':
+                    curr = file.read(1)
+            if curr == "*":
+                while curr!='/':
+                    curr = file.read(1)
+        elif(curr=='"'):
             #deal with string
-            pass
-        if(curr in SPECIALSTOP):
+            ret+=curr
+            curr = file.read(1)
+            while curr!='"':
+                ret+=curr
+                curr = file.read(1)
+            ret+=curr
+            curr = file.read(1) #Get breakcharacter (:,)
+            ret+=curr
+            break
+        elif(curr in SPECIALSTOP):
             #end funct and signal to builder to build something different
-            pass
-        if(curr in BREAKCHARS):
+            ret+=curr
+            break
+        elif(curr in BREAKCHARS):
             #end funct and signal to builder to handle token
-            pass
-
+            ret+=curr
+            break
+        else:
+            ret+=curr
     return ret
+
+def eatFunction(file)->None:
+    curr = ''
+    while curr!='}':
+        curr = file.read(1)
+    return
 
 """
 Needs to take in a file and a dict
@@ -35,3 +57,12 @@ def builddict(file: '_io.TextIOWrapper', target: dict) -> dict:
 #Handle lists as a special case for simplicity
 def buildlist(file: "_io.TextIOWrapper", target: list) -> list:
     pass
+
+if __name__ == "__main__":
+    target = input()
+    with open(target, "r+") as f:
+        t = getToken(f).strip()
+        while t!="":
+            print(t)
+            t = getToken(f).strip()
+    print("***************************DONE*****************************")
