3f8cb2587980cf112c948793c59baaa294adcd0a
decaquestionmarks <nateyheit@gmail.com>
2025-08-11 11:10:40 -0700

    buildlist still has bugs

--- dictInterpreter.py
+++ dictInterpreter.py
@@ -43,26 +43,62 @@ def eatFunction(file)->None:
         curr = file.read(1)
     return
 
+def eatLine(file) -> None:
+    curr = file.readline()
+    return
+
+def determineType(s: str)->type:
+    if(s.startswith('"')):
+        return str
+    elif(s=="true" or s=="false"):
+        return bool
+    elif(s.isnumeric()):
+        return int
+    elif(s.isalnum()):
+        try:
+            float(s)
+            return float
+        except ValueError:
+            pass
+    return str
+
 """
 Needs to take in a file and a dict
 Will interpret the dictionary as if it were a JSON
 ignore comments
 upon encountering another dictionary structue i.e. stats, learnsets, it will need to recursively call itself.
-return a copy?
+returns the input list
 
 """
-def builddict(file: '_io.TextIOWrapper', target: dict) -> dict:
+def builddict(file, target: dict) -> dict:
     pass
     
 #Handle lists as a special case for simplicity
-def buildlist(file: "_io.TextIOWrapper", target: list) -> list:
-    pass
+def buildlist(file, target: list) -> list:
+    token = " "
+    while token[-1]!="]":
+        token = getToken(file)
+        if token[-1]=="/":
+            while file.read(1)!='/':
+                pass
+        else:
+            t = determineType(token[:-1])
+            print("###"+token)
+            # print("###"+str(type(t)))
+            target.append(t(token[:-1].replace('"','').strip()))
+    return target
+        
 
 if __name__ == "__main__":
     target = input()
     with open(target, "r+") as f:
         t = getToken(f).strip()
+        if t.startswith("export"):
+            eatLine(f)
+            t = "{"
         while t!="":
+            if t=="[":
+                t = buildlist(f,[])
             print(t)
             t = getToken(f).strip()
     print("***************************DONE*****************************")
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import os
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
+from dictInterpreter import builddict
 load_dotenv()
 
 #dictionary population step
 file = open("Dawn/data/pokedex.ts","r+")
 basepokemon = builddict(file,dict())
 file = open("Dawn/data/mods/gen9sanctified/pokedex.ts","r+")
-pokemon = builddict(file,dict())
+pokemon = builddict(file,basepokemon.copy())
 
 
 
@@ -31,7 +32,7 @@ async def on_ready():
 
 @client.command(name='info', help='Tells you about the bot')
 async def info(ctx):
-    message = random.choice(["I'm the base 640 bot.", "Careful, I'm a bit of Klutz.", "Worst STABs since 2020.", "I wonder what I can't learn?"])
+    message = random.choice(["I'm the base 640 bot.", "Careful, I'm a bit of Klutz.", "Worst STABs since 2020.", "I wonder what I can't learn?", "Back and better than ever"])
     await ctx.channel.send(f"{ctx.author.mention}"+message)
 
 
